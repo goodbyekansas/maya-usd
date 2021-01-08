05cd113eb6c2f419d86b4a7d04458e7970b91db2
Dan Englesson <dan.englesson@goodbyekansas.com>
2021-01-08 18:17:04 +0100

    ⚡ removing build support for 2018 and adds support for usd 20

--- build_maya_usd.sh
+++ build_maya_usd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 scl enable devtoolset-6 bash
-echo y | rm -rf /usr/local/workspace2018
-echo "BUILDING maya-usd for maya 2018"
-CMAKE=cmake3 sudo python build.py --maya-location /usr/autodesk/maya2018 --v 3 --pxrusd-location /usr/local/USD.19/USD-core /usr/local/workspace2018 --install /usr/local/USD.19/maya-usd-2018
 echo y | rm -rf /usr/local/workspace2019
 echo "BUILDING maya-usd for maya 2019"
 CMAKE=cmake3 sudo python build.py --maya-location /usr/autodesk/maya2019 --v 3 --pxrusd-location /usr/local/USD.19/USD-core /usr/local/workspace2019 --install /usr/local/USD.19/maya-usd-2019
+echo y | rm -rf /usr/local/workspace2019
+echo "BUILDING maya-usd for maya 2019 usd v20.0"
+CMAKE=cmake3 sudo python build.py --maya-location /usr/autodesk/maya2019 --v 3 --pxrusd-location /usr/local/USD.20/USD-core /usr/local/workspace2019 --install /usr/local/USD.20/maya-usd-2019
+
+
 echo "DONE!"
